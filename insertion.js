2be8a831a57b10b047123d56be22a0e82f70d4ce
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-06-25 18:15:02 -0400

    Adding Insertion and Selection sort

--- insertion.js
+++ insertion.js
@@ -1,5 +1,12 @@
-function insertionSort() {
-    
+function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++){
+        let temp = arr[i];
+        for(j = i-1; j>=0 && arr[j] > temp; j--){
+            arr[j+1] = arr[j];
+        }
+        arr[j + 1] = temp;
+    }
+    return arr;
 }
 
 module.exports = insertionSort;
--- selection.js
+++ selection.js
@@ -1,3 +1,20 @@
-function selectionSort() {}
+function selectionSort(arr){
+    for(let i = 0; i < arr.length; i++){
+        let min = arr[i];
+        let key = i;
+        for(let j = i+1; j < arr.length; j++){ //scan for min
+            if(arr[j] < min){
+                min = arr[j];
+                key = j;
+            }
+        }
+        if(min < arr[i]) {      //swap min with selected
+            let temp = arr[i];
+            arr[i] = min;
+            arr[key] = temp;
+        }
+    }
+    return arr;
+}
 
 module.exports = selectionSort;
