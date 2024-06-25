9fb525f99b98d4870bbcffcbf0c0d533884de406
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-06-25 19:59:13 -0400

    Tweaking solution

--- selection.js
+++ selection.js
@@ -2,13 +2,13 @@ function selectionSort(arr){
     for(let i = 0; i < arr.length; i++){
         let min = arr[i];
         let key = i;
-        for(let j = i+1; j < arr.length; j++){ //scan for min
+        for(let j = i+1; j < arr.length; j++){
             if(arr[j] < min){
                 min = arr[j];
                 key = j;
             }
         }
-        if(min < arr[i]) {      //swap min with selected
+        if(min < arr[i]) {
             let temp = arr[i];
             arr[i] = min;
             arr[key] = temp;
